d2468da3032ee99be4cd9f35de31707a4daf0214
GitHubcyclic1 <113457649+GitHubcyclic1@users.noreply.github.com>
2022-09-15 12:44:16 +0530

    Update persiststorage.py

--- deploy-container/persiststorage.py
+++ deploy-container/persiststorage.py
@@ -17,12 +17,12 @@ os.system(f'touch /.config/rclone/rclone.conf')
 os.system(f'echo {data} | base64 -d > /.config/rclone/rclone.conf')
 print('config File Created')
 #Download
-os.system(f'rclone sync {remote}:{app_name} /app/WORKSPACE')
+os.system(f'rclone sync {remote}:{app_name} /home/coder/project')
 print('files Synced')
 os.system('pip install -r /app/WORKSPACE/.pip')
 print('Installed Python Packages in .pip File')
 #upload Every 1 min
 while True:
-    os.system(f'rclone sync  /app/WORKSPACE {remote}:{app_name}')
+    os.system(f'rclone sync  /home/coder/project {remote}:{app_name}')
     print('Files Uploaded')
     time.sleep(60)
